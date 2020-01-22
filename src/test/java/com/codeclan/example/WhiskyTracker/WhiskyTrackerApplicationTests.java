fd7eed2fdc237227e0d4b4c629b2c44a7a84fe6f
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-22 15:22:40 +0000

    findDistilleryByRegion added

--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
 import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
 import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
@@ -34,7 +35,10 @@ public class WhiskyTrackerApplicationTests {
 	}
 
 	@Test
-	public void findDistilleryByRegion()
+	public void findDistilleryByRegion() {
+		List<Distillery> found = distilleryRepository.findDistilleryByRegion("Highland");
+		assertEquals("Glendronach", found.get(0).getName());
+	}
 
 
 

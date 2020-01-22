84f5964650f76379dfca8d937ed8a7e32331dbf2
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-22 15:20:32 +0000

    initial commit

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepository.java
@@ -0,0 +1,16 @@
+package com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+
+@RepositoryRestResource
+public interface DistilleryRepository extends JpaRepository<Distillery, Long>, DistilleryRepositoryCustom {
+
+    List<Distillery> findDistilleryByRegion(String region );
+
+}
